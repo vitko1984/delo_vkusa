6a8992ce288c3eebd2b1280ca0a3c35711ed2a62
vitko1984 <knnaksay@gmail.com>
2022-08-05 08:10:59 +0300

    modify_about_prerender

--- src/routes/website/about.svelte
+++ src/routes/website/about.svelte
@@ -1,17 +1,17 @@
 <script context="module">
 	import { browser, dev } from '$app/env';
 
-	// we don't need any JS on this page, though we'll load
-	// it in dev so that we get hot module replacement...
+	// нам не нужен JS на этой странице, хотя мы загрузим
+	// это в dev, чтобы мы получили горячую замену модуля...
 	export const hydrate = dev;
 
-	// ...but if the client-side router is already loaded
-	// (i.e. we came here from elsewhere in the app), use it
+	// ...но если клиентский маршрутизатор уже загружен
+	// (т.е. мы пришли сюда из другого места в приложении), используйте его
 	export const router = browser;
 
-	// since there's no dynamic data here, we can prerender
-	// it so that it gets served as a static asset in prod
-	export const prerender = true;
+	// так как здесь нет динамических данных, мы можем пререндерить
+	// так, чтобы он служил статическим активом в prod
+	//export const prerender = true;
 </script>
 
 <svelte:head>
