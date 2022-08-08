13fc840f05d282d6304482fc17ffa29b783d428a
vitko1984 <knnaksay@gmail.com>
2022-08-08 17:58:52 +0300

    fix-6

--- netlify/functions/update.ts
+++ netlify/functions/update.ts
@@ -1,4 +1,13 @@
-exports.handler = async (event: { body: any; }) => {
+import { Handler } from "@netlify/functions";
+
+/*exports.handler = async (event: { body: any; }) => {
+  console.log("Received an update from Telegram!", event.body);
+  return { statusCode: 200 };
+};*/
+
+const handler: Handler = async (event, context) => {
   console.log("Received an update from Telegram!", event.body);
   return { statusCode: 200 };
 };
+  
+export { handler };
