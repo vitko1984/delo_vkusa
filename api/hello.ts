060833903f915ca69b6ed37bb03d5ddf6f1b0d13
vitko1984 <knnaksay@gmail.com>
2022-08-08 09:17:53 +0300

    fix-webhook

--- api/hello.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default const handlerWebhook = (request: VercelRequest, response: VercelResponse): void; new(): any; }; }; },) => {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
-  });
-}
--- api/webhook.ts
+++ api/webhook.ts
@@ -10,10 +10,11 @@ module.exports = async (request: VercelRequest, response: VercelResponse) => {
     // Использовать переменную окружения, чтобы не показывать ее в нашем коде
     //const bot = new TelegramBot(token, { polling: true });
     const bot = new TelegramBot('1479357668:AAFtfLRCDmRmDNyHAk16XXuqq18WsPu4OOc')
+    const userId = '1232247854';
     // Получаем тело POST-запроса, отправленного из Telegram
     const { body } = request;
     // Убедитесь, что это отправляемое сообщение
-    if (body.message) {
+    /*if (body.message) {
       // Получаем идентификатор этого чата
       // и текст, который отправил пользователь
       const { chat: { id }, text } = body.message;
@@ -22,6 +23,37 @@ module.exports = async (request: VercelRequest, response: VercelResponse) => {
       const message = `✅ Спасибо за ваше сообщение: *"${text}"*\nхорошего дня! 👋🏻`;
       // Отправляем наше новое сообщение обратно в Markdown
       await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
+    };*/
+    if (body.title === 'Заказ' || body.title === 'Перезвонить' || body.title === 'Комментарий "Контакты"') {
+      let html = '';
+      if (body.envelope) { 
+        let envlpCntnt = '';
+        body.envelope.map(v => {
+          envlpCntnt += `
+            Продукт: ${v.productName/*.replace(`|${locals.userid}`, '')*/}\n
+            Цена: ${v.price}\n
+            Количество: ${v.amount}`;
+        });
+        html = `*${body.title}*\n
+          Заказчик: ${body.name}\n 
+          Телефон: ${body.phone}\n 
+          Эл.почта: ${body.email}\n 
+          Адрес доставки: ${body.address}\n\n 
+          *Детали заказа:*\n
+          ${envlpCntnt}\n\n
+          *Общая стоимость:* ${req_data.total}`;
+      } else if (body.title === 'Перезвонить') {
+        html = `*${body.title}*\n
+          Клиент: ${body.name}\n
+          Телефон: ${body.phone}\n
+          Вемя звонка, пожелания: ${req_data.wish}`;	
+      } else if (body.title === 'Комментарий "Контакты"') {
+        html = `*${body.title}*\n
+          Клиент: ${body.name}\n
+          Эл.почта: ${body.email}\n
+          Комментарий, пожелание: ${body.wish}`;
+      };
+      bot.sendMessage(userId, html, {parse_mode: 'Markdown'});
     };
   } catch(error) {
     // Если произошла ошибка при отправке нашего сообщения, то мы
