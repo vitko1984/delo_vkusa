d8ba2e0ac3d496b1189b9c365ad67c1048dad218
vitko1984 <knnaksay@gmail.com>
2022-08-09 15:53:12 +0300

    fix-14

--- netlify/functions/update.ts
+++ netlify/functions/update.ts
@@ -1,16 +1,16 @@
 import { Handler } from "@netlify/functions";
-//import sendMessage from '../../middleware/sendMessage';
-import fetch from 'node-fetch';
+import sendMessage from '../../middleware/sendMessage';
+//import fetch from 'node-fetch';
 
 const handler: Handler = async (event, context) => {
   console.log("Получил обновление от Telegram!", event.body);
   const chatId = event.body && JSON.parse(event.body).message.chat.id;
   const text = event.body && JSON.parse(event.body).message.text;
-  //sendMessage(chatId, text);
-  const message = `✅ Спасибо за ваше сообщение: *"${text}"*\nхорошего дня! 👋🏻`; 
-  const url = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${chatId}&text=${message}&parse_mode=Markdown`;
-  const res = await fetch(url, {method: 'post'});
-  res.ok && console.log('Сеанс связи с телеграм прошел успешно...');
+  await sendMessage(chatId, text);
+  //const message = `✅ Спасибо за ваше сообщение: *"${text}"*\nхорошего дня! 👋🏻`; 
+  //const url = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${chatId}&text=${message}&parse_mode=Markdown`;
+  //const res = await fetch(url, {method: 'post'});
+  //res.ok && console.log('Сеанс связи с телеграм прошел успешно...');
   return { statusCode: 200 };
 };
   
