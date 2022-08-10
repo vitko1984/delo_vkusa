bcd2eb54c57ef196a82bef839f61f7025541e5e5
vitko1984 <knnaksay@gmail.com>
2022-08-10 10:20:58 +0300

    fix-23

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,10 +1,8 @@
-const handler = async (event: { headers: { [x: string]: any; }; }, _: any, callback: (arg0: null, arg1: { statusCode: number; body: any; }) => void) => {
-    const ip = event.headers['x-forwarded-for'];
-    const client = ip.split(',')[0];
-    callback(null, {
-      statusCode: 200,
-      body: client,
-    });
+const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Good morning!' }),
   };
+}; 
 
-  export { handler };
+export { handler };
